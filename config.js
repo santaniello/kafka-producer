1d09cd71c67cf119750bfb60be88f7e8b14011fe
Felipe Santaniello <felipe.santaniello-ext@viavarejo.com.br>
2019-10-04 13:46:39 -0300

    Commit inicial

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  kafka_topic: 'example',
-  kafka_server: 'localhost:2181',
+  kafka_topic: 'MARKETPLACE.PROMOCAO.ATUALIZADO',
+  kafka_server: '10.228.160.31:2181',
 };
--- producer.app.js
+++ producer.app.js
@@ -9,12 +9,16 @@ try {
   const producer = new Producer(client);
   const kafka_topic = 'example';
   console.log(kafka_topic);
-  let payloads = [
-    {
-      topic: kafka_topic,
-      messages: config.kafka_topic
-    }
-  ];
+let teste = JSON.parse('{"RULEID":"60850","NAME":"FreteGrátis_SP","DATAINICIAL":"12/06/13","DATAFINAL":"","LOJISTA":"NCMP_10030","LAST_UPDATE":"07/04/16 05:32:14,000000","TIPO":"Benefício","IDTIPO":"91","DESCATRIBUTO":"Desconto","IDOPERACAO":"20","DESCOPERACAO":"igual a","OPERACAO":"==","VALUE":"100"}');
+
+let payloads = [
+   {
+    topic: kafka_topic,
+    messages: '{"RULEID":"60850","NAME":"FreteGrátis_SP","DATAINICIAL":"12/06/13","DATAFINAL":"","LOJISTA":"NCMP_10030","LAST_UPDATE":"07/04/16 05:32:14,000000","TIPO":"Benefício","IDTIPO":"91","DESCATRIBUTO":"Desconto","IDOPERACAO":"20","DESCOPERACAO":"igual a","OPERACAO":"==","VALUE":"100"}'
+   }
+];
+
+  console.log(payloads)
 
   producer.on('ready', async function() {
     let push_status = producer.send(payloads, (err, data) => {
